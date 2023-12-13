7ab9c7eb4e4db22e44663b58c1855bbd1a97a43d
Ryan <ryandanver@gmail.com>
2023-12-13 12:03:22 -0800

    day 7 p1

--- solutions/day7/day7.py
+++ solutions/day7/day7.py
@@ -3,21 +3,46 @@ import sys
 text = open(sys.argv[1]).readlines()
 hands = [line.strip().split(' ') for line in text]
 
-card_chars = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-cards = {}
-for i in range(len(card_chars)-1, -1, -1):
-    cards[card_chars[i]] = len(card_chars) - i
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 
-def hand_typerator(h):
-    occurs = {}
-    for char in h[0]:
-        if char not in occurs:
-            occurs[char] = 0
-        occurs[char] += 1
-    occurs = dict(sorted(occurs.items(), key=lambda item: item[1]))
-    print(occurs)
+card_map = {}
+for i in range(0, len(cards)):
+    card_map[cards[i]] = i
 
-hand_typerator(hands[0])
+def hand_type(hand):
+    counts = {}
+    for c in hand:
+        if c not in counts:
+            counts[c] = 0
+        counts[c] += 1
+    counts = sorted(counts.values(), reverse=True)
+    if counts[0] == 5: return 6
+    if counts[0] == 4: return 5
+    if counts[0] == 3: return 4 if len(counts) == 2 else 3
+    if counts[0] == 2: return 2 if counts[1] == 2 else 1
+    return 0
 
 for h in hands:
-    pass
+    h.append(hand_type(h[0]))
+hands.sort(key=lambda h: h[2])
+
+swap = True
+while swap:
+    swap = False
+    for i in range(len(hands)-1):
+        (h1, h2) = (hands[i], hands[i+1])
+        if h1[2] == h2[2]:
+            for j in range(len(h1[0])):
+                if card_map[h1[0][j]] > card_map[h2[0][j]]:
+                    t = hands[i]
+                    hands[i] = hands[i+1]
+                    hands[i+1] = t
+                    swap = True
+                    break
+                elif h1[0][j] != h2[0][j]:
+                    break
+
+winnings = 0
+for i in range(len(hands)):
+    winnings += (i+1) * int(hands[i][1])
+print("Part one:", winnings)
