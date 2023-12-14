8a2f1f36543ce8156d3310a7232999eefc210ae2
Ryan <ryandanver@gmail.com>
2023-12-13 20:31:49 -0800

    day 9 p2

--- solutions/day9/day9.py
+++ solutions/day9/day9.py
@@ -6,8 +6,18 @@ def get_next(seq: list) -> int:
     diffs = [seq[i+1] - seq[i] for i in range(len(seq)-1)]
     return seq[-1] if all(n == 0 for n in diffs) else seq[-1] + get_next(diffs)
 
+# part 1
 total = 0
 for line in text:
     seq = [int(n) for n in line.strip().split(' ')]
     total += get_next(seq)
-print("Part one", total)
+
+print("Part one:", total)
+
+# part 2
+total = 0
+for line in text:
+    seq = reversed([int(n) for n in line.strip().split(' ')])
+    total += get_next(list(seq))
+    
+print("Part two:", total)
